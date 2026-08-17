0a3e3bf5d9ed18778a2adeeb5b48afe196900117
Dydy1212-r <vinvandy5@gmail.com>
2026-08-18 03:26:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ import { createClient } from
 // ========================================
 
 const SUPABASE_URL =
-  "YOUR_SUPABASE_PROJECT_URL";
+  "https://glpkadgmsaozmcebyrxw.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "YOUR_SUPABASE_PUBLISHABLE_KEY";
+  "sb_publishable_UYD86Z2gQD5o8BMfuP5IHw__bIcUX0C";
 
 
 // ========================================
